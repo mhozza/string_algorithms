4ff75f958a6cae16f77b050ef49a2c1ceee0a601
Michal Hozza <mhozza@gmail.com>
2016-05-10 19:43:40 +0200

    added aho-corrasick

---
+++ string_algorithms/aho_corrasick.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+from .trie import TrieNode, Trie
+
+
+class ACNode(TrieNode):
+    def __init__(self, *args, **kwargs):
+        super(ACNode, self).__init__(*args, **kwargs)
+        self.output = set()
+        self.fail = None
+
+
+def preprocess(patterns):
+    pattern_map = dict()
+    t = Trie(node_class=ACNode)
+    for pattern in patterns:
+        pattern_map[t.add(pattern)] = pattern
+    q = deque([t.root])
+
+    while q:
+        parent = q.popleft()
+        for c, node in parent.children.items():
+            fail = parent.fail
+            while fail and fail != t.root and c not in fail.children:
+                fail = fail.fail
+            if fail and c in fail.children:
+                node.fail = fail.children[c]
+            else:
+                node.fail = t.root
+            if node.is_word:
+                node.output = set([node]) | node.fail.output
+            else:
+                # copy output from fail link
+                node.output = set(node.fail.output)
+            q.append(node)
+    return t, pattern_map
+
+
+def find(patterns, text):
+    t, pattern_map = preprocess(patterns)
+    results = []
+    node = t.root
+    for i, c in enumerate(text):
+        while node != t.root and c not in node.children:
+            node = node.fail
+        if c in node.children:
+            node = node.next(c)
+            results += [(i - len(pattern_map[o]) + 1, pattern_map[o]) for o in node.output]
+    return results
---
+++ tests/test_aho_corrasick.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+
+from string_algorithms.aho_corrasick import preprocess, find
+from string_algorithms.naive_find import find as naive_find
+
+
+class TestAhoCorrasickPreprocess(unittest.TestCase):
+    def test_preprocess(self):
+        patterns = ('at', 'gag', 'gc', 'gata')
+        t, _ = preprocess(patterns)
+        self.assertEqual(t.get_node('ga').fail, t.get_node('a'))
+        self.assertEqual(t.get_node('gag').fail, t.get_node('g'))
+        self.assertEqual(t.get_node('gat').fail, t.get_node('at'))
+        self.assertEqual(t.get_node('gata').fail, t.get_node('a'))
+        self.assertEqual(t.get_node('at').fail, t.root)
+        self.assertEqual(t.get_node('a').fail, t.root)
+
+
+class TestAhoCorrasick(unittest.TestCase):
+    def test_find(self):
+        patterns = ('at', 'gag', 'gc', 'gata')
+        text = 'tatatattgcgccatattagagattagatagga'
+        result = set()
+        for pattern in patterns:
+            occurrences = naive_find(pattern, text)
+            result |= set(((o, pattern) for o in occurrences))
+
+        self.assertSetEqual(result, set(find(patterns, text)))
