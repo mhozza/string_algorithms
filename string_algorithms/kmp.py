2afa8a46149325d96225d773b96ddc80bb7be6aa
Michal Hozza <mhozza@gmail.com>
2016-05-10 12:08:08 +0200

    KMP

---
+++ string_algorithms/kmp.py
@@ -0,0 +1,25 @@
+def find(pattern, text):
+    # Compute fail function
+    p = [0] * len(pattern)
+    j = 0
+    for i in range(1, len(pattern)):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = p[j - 1]
+        if pattern[i] == pattern[j]:
+            j += 1
+            p[i] = j
+    print(list(zip(pattern, p)))
+    # find
+    results = []
+    j = 0
+    for i in range(0, len(text)):
+        while j > 0 and text[i] != pattern[j]:
+            j = p[j - 1]
+            print(i, j)
+        if text[i] == pattern[j]:
+            if j == len(pattern) - 1:
+                results.append(i - j)
+                j = p[j - 1]
+            j += 1
+
+    return results
