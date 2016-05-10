8c8e149347f1c65dd59a7347234b1cf11fd0e38e
Michal Hozza <mhozza@gmail.com>
2016-05-10 14:02:37 +0200

    kmp fixes

--- string_algorithms/kmp.py
+++ string_algorithms/kmp.py
@@ -8,14 +8,12 @@ def find(pattern, text):
         if pattern[i] == pattern[j]:
             j += 1
             p[i] = j
-    print(list(zip(pattern, p)))
     # find
     results = []
     j = 0
     for i in range(0, len(text)):
         while j > 0 and text[i] != pattern[j]:
             j = p[j - 1]
-            print(i, j)
         if text[i] == pattern[j]:
             if j == len(pattern) - 1:
                 results.append(i - j)
