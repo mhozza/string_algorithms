2a9cc9ca80adb5f7f51c76b531a61eebf3aacd4d
Michal Hozza <mhozza@gmail.com>
2016-05-24 14:30:37 +0200

    first version of LCA

---
+++ string_algorithms/lca.py
@@ -0,0 +1,25 @@
+from .tree import preorder_traversal
+from .rmq import RMQ
+
+
+class LCA:
+    def __init__(self, tree):
+        self.tree = tree
+        self.euler_tour = []
+        self.depths = []
+        self.representative = dict()
+        preorder_traversal(self.tree, self.compute_arrays_action)
+        self.compute_representatives()
+        self.rmq = RMQ(self.depths)
+
+    def compute_arrays_action(self, node, depth):
+        self.euler_tour.append(node)
+        self.depths.append(depth)
+
+    def compute_representatives(self):
+        for i, n in enumerate(self.euler_tour):
+            if n not in self.representative:
+                self.representative[n] = i
+
+    def query(self, a, b):
+        return self.euler_tour[self.rmq.query(self.representative[a], self.representative[b])]
