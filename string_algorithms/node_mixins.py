2e1a26142d3c3caf13ceec3328b0ecf9a8fe84c7
Michal Hozza <mhozza@gmail.com>
2016-05-24 16:09:10 +0200

    added labeled node mixin

---
+++ string_algorithms/node_mixins.py
@@ -0,0 +1,15 @@
+class LabeledNode:
+    def __init__(self, label, *args, **kwargs):
+        self.label = label
+        super(LabeledNode, self).__init__(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.label)
+
+    def add(self, key, label=None, *args, **kwargs):
+        if label is None:
+            label = key
+        return super(LabeledNode, self).add(key, label=label)
+
+    def __lt__(self, other):
+        return self.label < other.label
