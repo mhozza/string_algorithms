ace2c77d16fafcfa9a6ca687430d3c564494d6a6
Michal Hozza <mhozza@gmail.com>
2016-05-27 21:22:15 +0200

    moved string matching algorithm into one module

--- string_algorithms/exact_string_matching.py
+++ string_algorithms/exact_string_matching.py
@@ -3,6 +3,46 @@ from collections import deque
 from .trie import Trie, TrieNode
 
 
+def naive_find(pattern, text):
+    results = []
+    for i in range(len(text) - len(pattern)):
+        found = True
+        for j, p in enumerate(pattern):
+            if text[i + j] != p:
+                found = False
+                break
+        if found:
+            results.append(i)
+    return results
+
+
+def kmp_preprocess(pattern):
+    p = [0] * len(pattern)
+    for i in range(1, len(pattern)):
+        j = p[i - 1]
+        while j > 0 and pattern[i] != pattern[j]:
+            j = p[j - 1]
+        if pattern[i] == pattern[j]:
+            p[i] = j + 1
+    return p
+
+
+def kmp(pattern, text):
+    p = kmp_preprocess(pattern)
+    results = []
+    j = 0
+    for i, c in enumerate(text):
+        while j > 0 and c != pattern[j]:
+            j = p[j - 1]
+        if c == pattern[j]:
+            if j == len(pattern) - 1:
+                results.append(i - j)
+                j = p[j - 1]
+            else:
+                j += 1
+    return results
+
+
 class ACNode(TrieNode):
     def __init__(self, *args, **kwargs):
         super(ACNode, self).__init__(*args, **kwargs)
@@ -10,7 +50,7 @@ class ACNode(TrieNode):
         self.fail = None
 
 
-def preprocess(patterns):
+def aho_corrasick_preprocess(patterns):
     pattern_map = dict()
     t = Trie(node_class=ACNode)
     for pattern in patterns:
@@ -37,8 +77,8 @@ def preprocess(patterns):
     return t, pattern_map
 
 
-def find(patterns, text):
-    t, pattern_map = preprocess(patterns)
+def aho_corrasick(patterns, text):
+    t, pattern_map = aho_corrasick_preprocess(patterns)
     results = []
     node = t.root
     for i, c in enumerate(text):
---
+++ tests/test_exact_string_matching.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+from string_algorithms.exact_string_matching import naive_find, kmp, kmp_preprocess, aho_corrasick, aho_corrasick_preprocess
+
+
+class TestNaiveFind(unittest.TestCase):
+    def test_find(self):
+        text = 'tatatattgcgccatattagagattagatagga'
+        self.assertListEqual(naive_find('at', text), [1, 3, 5, 13, 15, 22, 27])
+        self.assertListEqual(naive_find('gag', text), [19])
+        self.assertListEqual(naive_find('gc', text), [8, 10])
+        self.assertListEqual(naive_find('gata', text), [26])
+
+
+class TestKMPPreprocess(unittest.TestCase):
+    def test_empty(self):
+        pattern = ''
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), 0)
+
+    def test_tiny(self):
+        pattern = 'a'
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), 1)
+        self.assertListEqual(p, [0])
+
+    def test_small(self):
+        pattern = 'at'
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), 2)
+        self.assertListEqual(p, [0, 0])
+
+    def test_small2(self):
+        pattern = 'aa'
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), 2)
+        self.assertListEqual(p, [0, 1])
+
+    def test_repetitive(self):
+        pattern = 'tatata'
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), len(pattern))
+        self.assertListEqual(p, [0, 0, 1, 2, 3, 4])
+
+    def test_repetitive2(self):
+        pattern = 'aaaaaa'
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), len(pattern))
+        self.assertListEqual(p, [0, 1, 2, 3, 4, 5])
+
+    def test_almost_repetitive(self):
+        pattern = 'ananas'
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), len(pattern))
+        self.assertListEqual(p, [0, 0, 1, 2, 3, 0])
+
+    def test_nonrepetitive(self):
+        pattern = 'abcdefgh'
+        p = kmp_preprocess(pattern)
+        self.assertEqual(len(p), len(pattern))
+        self.assertListEqual(p, [0] * len(pattern))
+
+
+class TestKMP(unittest.TestCase):
+    def test_small(self):
+        patterns = ('at', 'gag', 'gc', 'gata')
+        text = 'tatatattgcgccatattagagattagatagga'
+        for pattern in patterns:
+            self.assertListEqual(kmp(pattern, text), naive_find(pattern, text))
+
+
+class TestAhoCorrasickPreprocess(unittest.TestCase):
+    def test_preprocess(self):
+        patterns = ('at', 'gag', 'gc', 'gata')
+        t, _ = aho_corrasick_preprocess(patterns)
+        self.assertEqual(t.get_node('ga').fail, t.get_node('a'))
+        self.assertEqual(t.get_node('gag').fail, t.get_node('g'))
+        self.assertEqual(t.get_node('gat').fail, t.get_node('at'))
+        self.assertEqual(t.get_node('gata').fail, t.get_node('a'))
+        self.assertEqual(t.get_node('at').fail, t.root)
+        self.assertEqual(t.get_node('a').fail, t.root)
+
+
+class TestAhoCorrasick(unittest.TestCase):
+    def test_find(self):
+        patterns = ('at', 'gag', 'gc', 'gata')
+        text = 'tatatattgcgccatattagagattagatagga'
+        result = set()
+        for pattern in patterns:
+            occurrences = naive_find(pattern, text)
+            result |= set(((o, pattern) for o in occurrences))
+
+        self.assertSetEqual(result, set(aho_corrasick(patterns, text)))
